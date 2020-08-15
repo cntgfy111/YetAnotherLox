972bb68b3a113c415f3f1f667e6bdb21d1d39ebd
thecntgfy <awanStep@ya.ru>
2020-08-15 18:44:43 +0300

    Added AST to LoxI

---
+++ Lox/src/com/cntgfy/lox/PolishNotationAstPrinter.java
@@ -0,0 +1,50 @@
+package com.cntgfy.lox;
+
+public class PolishNotationAstPrinter implements Expr.Visitor<String> {
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal(45.67)));
+
+        System.out.println(new PolishNotationAstPrinter().print(expression));
+    }
+
+    String print(Expr expr) { return expr.accept(this); }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parethesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parethesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parethesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String parethesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append('(');
+        for (Expr expr : exprs) {
+            builder.append(expr.accept(this)).append(' ');
+        }
+        builder.append(name).append(')');
+
+        return  builder.toString();
+    }
+}
